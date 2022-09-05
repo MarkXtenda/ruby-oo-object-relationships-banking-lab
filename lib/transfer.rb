82be98c4338c86f3c6c2c5b47f913a025ec52825
MarkXtenda <mark.i2p.1998@gmail.com>
2022-09-05 23:15:41 +1000

    done

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
 class Transfer
-  # your code here
+    attr_reader :sender, :receiver, :amount
+    attr_accessor :status
+    def initialize(sender, receiver, amount)
+        @sender = sender
+        @receiver = receiver
+        @amount = amount
+        @status = 'pending'
+    end
+
+    def valid?
+      (sender.valid? && receiver.valid?) ? true : false
+    end
+
+    def execute_transaction
+      puts @sender.valid?
+      if (@status == 'pending' && @sender.valid? && @receiver.valid? && @sender.balance >= amount)
+        @sender.deposit(-amount)
+        @receiver.deposit(amount)
+        @status = "complete"
+      else 
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+
+    def reverse_transfer
+      if (@status == 'complete' && @receiver.valid?)
+        @receiver.deposit(-amount)
+        @sender.deposit(amount)
+        @status = "reversed"
+      end 
+    end
 end
